6cfbfa0ca15d44c05cc7357aa1896b7c5162880f
Kaustubh Harapanahalli <kaustubhharapanahalli@gmail.com>
2024-10-17 23:05:52 -0700

    Add homepage view

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Homepage
+from .serializers import HomepageSerializer
+
+
+class HomepageView(generics.RetrieveAPIView):
+    """
+    HomepageView is a RetrieveAPIView that retrieves the first object from the Homepage queryset.
+    Attributes:
+        queryset (QuerySet): A Django QuerySet containing all Homepage objects.
+        serializer_class (Serializer): The serializer class used to serialize the Homepage objects.
+    Methods:
+        get_object():
+    """
+
+    queryset = Homepage.objects.all()
+    serializer_class = HomepageSerializer
+
+    def get_object(self):
+        """
+        Retrieve the first object from the queryset.
+        Returns:
+            object: The first object in the queryset, or None if the queryset is empty.
+        """
+
+        return self.queryset.first()
